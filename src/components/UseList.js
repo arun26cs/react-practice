ba55af440887d5594e6d02810f1f1f998c5124f8
arun <arun26cs@gmail.com>
2020-09-28 01:42:23 -0400

    add changes

--- src/components/UseList.js
+++ src/components/UseList.js
@@ -1,23 +1,22 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-class UserList extends Component{
-    render(){
-        console.log('state',this.state);
-        console.log('props',this.props);
-        const users = this.props.users;
-        const userlistmap = users.map(user =>{
-            return (
-                <p key={user.id}> this is {user.name} aged {user.age} having {user.belt} belt</p>
-            );
-        })
+const UserList = (props) => {
+    const users = props.users; 
+    console.log('props', props);
+    //const users = this.props.users;
+    const userlistmap = users.map(user => {
         return (
-            <div>   
-                <h1>User data</h1>
-                {userlistmap}
-            </div>
-        
+            <p key={user.id}> this is {user.name} aged {user.age} having {user.belt} belt</p>
         );
-    }
-} 
+    })
+    return (
+        <div>
+            <h1>User data</h1>
+            {userlistmap}
+        </div>
+
+    );
+
+}
 
 export default UserList;
